fd2fa229fc04bcfe9e263426130a88d0cba3eb02
Taras Chirkov <chirkovtaras@gmail.com>
2021-03-22 13:24:13 +0200

    Updates

---
+++ precache-manifest.fd79028806a08a6e6055fe7df66023fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5c70958a5947b0627325e32ed5d9f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7ce5673ba8d501a878f",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "828bb0aaf9acd9abc8c4",
+    "url": "./static/js/2.278a2e2b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.278a2e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ce5673ba8d501a878f",
+    "url": "./static/js/main.3cfc31fa.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
